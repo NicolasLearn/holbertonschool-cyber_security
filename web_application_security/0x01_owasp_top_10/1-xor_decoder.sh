48ceaad7644ed74d1b685c6c273d6f7ff9a531b4
Nicolas <nicolas.quoirez@hotmail.fr>
2024-10-14 21:25:47 +0200

    task1, try with python

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -5,26 +5,23 @@ if [ -z "$1" ]; then
     echo "Usage: $0 {xor}<base64_string>"
     exit 1
 fi
+
 # Supprime le préfixe "{xor}"
 password="${1#'{xor}'}"
-# Décode la chaîne encodée en Base64 en supprimant les lignes vides
+
+# Décode la chaîne encodée en Base64
 decoded_password=$(echo -n "$password" | base64 --decode | tr -d '\0')
-# Vérifie si le décodage Base64 a réussi
+
 if [ $? -ne 0 ]; then
     echo "Erreur : Décodage Base64 échoué."
     exit 1
 fi
-# Initialise une variable pour stocker le résultat
-output=""
-# Parcourt chaque caractère de la chaîne décodée
-for ((i = 0; i < ${#decoded_password}; i++)); do
-    char="${decoded_password:$i:1}"
 
-    # Effectue l'opération XOR avec 95 (en supprimant les octets nuls)
-    xor_result=$(( $(printf "%d" "'$char") ^ 95 ))
-
-    # Ajoute le résultat final à la sortie
-    output+=$(printf "\\$(printf '%03o' $xor_result)")
-done
-# Affiche le résultat décrypté
-echo "$output"
+# Utilise Python pour effectuer l'opération XOR avec la clé 95
+python3 -c "
+import sys
+decoded_password = '$decoded_password'
+key = 95
+output = ''.join([chr(ord(c) ^ key) for c in decoded_password])
+print(output)
+"
